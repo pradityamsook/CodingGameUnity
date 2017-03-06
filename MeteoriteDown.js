fe1c24ae13c5e12e50b8fed5769513bf6b59fdaa
pradityamsook <praditcry@gmail.com>
2017-03-07 00:22:40 +0700

    Add files via upload

---
+++ MeteoriteDown.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var speedDown : float = 0.1f;
+private var down : Rigidbody2D;
+
+function Start(){
+	down = GetComponent.<Rigidbody2D>();
+}
+
+function Update(){
+	downMet();
+}
+
+function downMet(){
+
+	var position2 : Vector2 = down.transform.position;
+	position2.y -= speedDown;
+	down.transform.position = position2;
+
+		if(position2.y < 9){
+			Destroy(gameObject);
+		}
+}
