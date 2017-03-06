dabacbe0cc4c3be86ff818f8c0b15a2f0b23ebfe
pradityamsook <praditcry@gmail.com>
2017-03-07 00:19:44 +0700

    Add files via upload

---
+++ Meteorite.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+var time  = 0.0;
+var Meteorite : GameObject;
+
+var randomStart : float = 0;
+var randomEnd : float = 0;
+private var randomO : float;
+
+function Start () {
+
+}
+
+function Update (){
+	Meteorites();
+}
+
+function Meteorites () {
+	time += Time.deltaTime;
+	if(time >= 1){
+		time = 0;
+		var position : Vector3 = new Vector3();
+		position.x = Random.Range(randomStart,randomEnd);
+		position.y = 18.95;
+		Instantiate(Meteorite, position,Quaternion.identity);
+
+	}
+
+}
+function OnTriggerEnter2D(mothur : Collider2D){
+	if (mothur.gameObject.CompareTag("Rocket")){
+		Destroy(mothur.gameObject);
+	}
+}
---
+++ RocketLaucher.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var speedRocket : float = 0.1f;
+
+function Start () {
+
+}
+
+function Update () {
+	RocketLaucher();
+}
+
+function RocketLaucher () {
+
+	 var position : Vector2 = transform.position;
+	 position.y += speedRocket;
+	 transform.position = position;
+
+	 if (position.y > 18 ){
+	 	Destroy(gameObject);
+	 }
+}
